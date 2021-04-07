347a24a0597af56677583db55eeb83b1cea66182
xi0806 <xi0806@126.com>
2021-04-07 22:22:27 +0800

    1

---
+++ Assets/Scripts/DropLootWhenDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A2
+{
+    public class DropLootWhenDeath : MonoBehaviour
+    {
+        public GameObject healthPack;
+        public HealthState healthState;
+        void Start()
+        {
+            healthState.actionDeath += Death;
+        }
+
+        void Death()
+        {
+            int count = Random.Range(0,3);
+            for(int i=0;i<count;i++)
+            {
+                var obj = Instantiate(healthPack);
+                obj.transform.position = transform.position;
+            }
+        }
+    }
+}
